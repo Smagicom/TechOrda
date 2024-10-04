1a72805d42f35e2e0e2e359f3636a443ebd86af5
Dimash <dimashaijarikov@gmail.com>
2024-10-05 04:10:37 +0500

    didn't solved power-a-b.py

---
+++ python/results/power-a-b.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+if b > 0:
+    print("A^B is not equal to 0")
+else:
+    for b in range(a):
+        print(a * b)
--- python/results/print-even-a-b.py
+++ python/results/print-even-a-b.py
@@ -5,3 +5,4 @@ for num in range(a, b + 1):
     if num % 2 == 0:  
         print(num, end=' ')  
 print()
+    
