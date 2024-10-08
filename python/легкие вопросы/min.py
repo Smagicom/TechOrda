32c0fd7f087feda205324ec75e362e4c4da190af
aman <mnkapezov@gmail.com>
2024-10-08 11:37:18 +0500

    Add some changes in min.py

--- python/легкие вопросы/min.py
+++ python/легкие вопросы/min.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 def min_num(nums):
-    if not nums:  
-        return None
-    res = nums[0]
-    for i in nums:
-         if i < res:
-             res = i
-    return res 
+    if 0 <= len(nums) <= 5:
+        if not nums:  
+            print("The list is empty!")
+            return None
+        res = nums[0]
+        for i in nums:
+           if i < res:
+              res = i
+        return res
+    else:
+        print("The numbers out of range!") 
           
 num_input = input("Please enter the numbers, there must be spaces between the numbers.: ")
 numbers = list(map(int, num_input.split()))
