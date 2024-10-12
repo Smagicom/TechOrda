47d985c2e3a4e19c17afe684d5409ef46440d3c2
aman <mnkapezov@gmail.com>
2024-10-12 16:23:46 +0500

    Add code for the task perfect number

---
+++ python/средние задачи/perfectnum.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def perfectnum(n):
+    n = int(n)
+    sum = 0
+    if n <= 0:
+        return "The number must be greater than 0!"
+    for i in range(1, n):
+        if n % i == 0:
+            sum += i
+    if sum == n:
+        return "Perfect number"
+    else:
+        return "Not a perfect number"
+    
+num = input("Please enter the number: ")
+print(perfectnum(num))
